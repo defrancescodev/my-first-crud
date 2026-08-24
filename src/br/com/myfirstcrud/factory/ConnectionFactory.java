7e499be6657bf87f8010ced8070c45b7f58b4ace
Samuel De Francesco <defrancescodev@gmail.com>
2026-08-24 10:34:18 -0300

    Connection to database is working

---
+++ src/br/com/myfirstcrud/factory/ConnectionFactory.java
@@ -0,0 +1,37 @@
+package br.com.myfirstcrud.factory;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+    //Conexão do tipo JDBC
+
+    private static final String USERNAME = "root";
+    private static final String PASSWORD= "1234";
+    private static final String DATABASE_URL = "jdbc:mysql://localhost:3306/games";
+
+    /**
+     * Conexão com o banco de dados
+     */
+
+    public static Connection connection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver"); //Faz com que a classe seja carregada pela JVM
+
+        return DriverManager.getConnection(DATABASE_URL, USERNAME, PASSWORD);
+    }
+
+    public static void main(String[] args) {
+        try {
+            Connection connection = connection();
+
+            if (connection != null) {
+                System.out.println("Conexão obetida com sucesso");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
