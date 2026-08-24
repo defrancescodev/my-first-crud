f8e565de43f403aefb3e3c7a7bdd841939434300
Samuel De Francesco <defrancescodev@gmail.com>
2026-08-24 15:57:21 -0300

    feat(Games): Filled class with attributes and methods

--- src/br/com/myfirstcrud/model/Games.java
+++ src/br/com/myfirstcrud/model/Games.java
@@ -1,5 +1,60 @@
 package br.com.myfirstcrud.model;
 
-public class Games {
+import java.util.Date;
 
+public class Games {
+    private int id;
+    private String title;
+    private Date releaseYear;
+    private String developer;
+    private boolean ownParryMechanic;
+    private String VisualThemaic;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(Date releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public String getDeveloper() {
+        return developer;
+    }
+
+    public void setDeveloper(String developer) {
+        this.developer = developer;
+    }
+
+    public boolean isOwnParryMechanic() {
+        return ownParryMechanic;
+    }
+
+    public void setOwnParryMechanic(boolean ownParryMechanic) {
+        this.ownParryMechanic = ownParryMechanic;
+    }
+
+    public String getVisualThemaic() {
+        return VisualThemaic;
+    }
+
+    public void setVisualThemaic(String visualThemaic) {
+        VisualThemaic = visualThemaic;
+    }
 }
