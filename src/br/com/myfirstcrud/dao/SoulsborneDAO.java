dadd997092d2d53f5aa54bceee803c8d40b03d68
Samuel De Francesco <defrancescodev@gmail.com>
2026-08-26 13:47:32 -0300

    SoulsborneDAO new method selects all games from my database

--- src/br/com/myfirstcrud/dao/SoulsborneDAO.java
+++ src/br/com/myfirstcrud/dao/SoulsborneDAO.java
@@ -6,6 +6,9 @@ import br.com.myfirstcrud.model.Soulsborne;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SoulsborneDAO {
 
@@ -14,7 +17,6 @@ public class SoulsborneDAO {
 
         Connection con = null;
         PreparedStatement pstm = null;
-
         try {
             con = ConnectionFactory.createConnection();
             pstm = (PreparedStatement) con.prepareStatement(sql);
@@ -23,24 +25,72 @@ public class SoulsborneDAO {
             pstm.setInt(3, soulsborne.getRelease_year());
             pstm.setBoolean(4, soulsborne.isOwn_parry_mechanic());
             pstm.setString(5, soulsborne.getVisual_thema());
-
             pstm.execute();
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
-             try {
-                 if (pstm != null) {
-                     pstm.close();
-                 }
+            try {
+                if (pstm != null) {
+                    pstm.close();
+                }
 
-                 if (con != null) {
-                     pstm.close();
-                 }
-             } catch (Exception e) {
-                 e.printStackTrace();
-             }
+                if (con != null) {
+                    pstm.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public List<Soulsborne> getSoulsbornes() {
+        String sql = "SELECT * FROM soulsborne";
+
+        List<Soulsborne> soulsbornes = new ArrayList<Soulsborne>();
+        Connection con = null;
+        PreparedStatement pstm = null;
+        ResultSet rset = null; //Classe que vai recuperar os dandos do banco ***SELECT***
+
+        try {
+            con = ConnectionFactory.createConnection();
+            pstm = (PreparedStatement) con.prepareStatement(sql);
+            rset = pstm.executeQuery();
+
+            while (rset.next()) {
+                Soulsborne soulsborne = new Soulsborne();
+                soulsborne.setId(rset.getInt("id"));
+                soulsborne.setTitle(rset.getString("title"));
+                soulsborne.setDeveloper(rset.getString("developer"));
+                soulsborne.setRelease_year(rset.getInt("release_year"));
+                soulsborne.setOwn_parry_mechanic(rset.getBoolean("own_parry_mechanic"));
+                soulsborne.setVisual_thema(rset.getString("visual_thema"));
+
+                soulsbornes.add(soulsborne);
+            }
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+
+                if (con != null) {
+                    con.close();
+                }
+
+                if (pstm != null) {
+                    pstm.close();
+                }
+
+                if (rset != null) {
+                    rset.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
 
+        return soulsbornes;
     }
 
 }
--- src/br/com/myfirstcrud/main/Main.java
+++ src/br/com/myfirstcrud/main/Main.java
@@ -6,16 +6,27 @@ import br.com.myfirstcrud.model.Soulsborne;
 public class Main {
     public static void main(String[] args) {
         SoulsborneDAO soulsborneDAO = new SoulsborneDAO();
-        Soulsborne soulsborne = new Soulsborne();
+        Soulsborne soulsborne1 = new Soulsborne();
 
-        soulsborne.setId(2);
-        soulsborne.setTitle("Dark Souls 2");
-        soulsborne.setDeveloper("FromSoftware");
-        soulsborne.setRelease_year(2014);
-        soulsborne.setOwn_parry_mechanic(true);
-        soulsborne.setVisual_thema("Dark Fantasy");
+        //soulsborne1.setId(3);
+        //soulsborne1.setTitle("Dark Souls 3");
+        //soulsborne1.setDeveloper("FromSoftware");
+        //soulsborne1.setRelease_year(2016);
+        //soulsborne1.setOwn_parry_mechanic(true);
+        //soulsborne1.setVisual_thema("Ruined World");
 
-        soulsborneDAO.save(soulsborne);
+        //soulsborneDAO.save(soulsborne);
+
+       for (Soulsborne soulsborne : soulsborneDAO.getSoulsbornes()) {
+           System.out.println(soulsborne.getTitle());
+           System.out.println(soulsborne.getDeveloper());
+           System.out.println(soulsborne.getRelease_year());
+           System.out.println(soulsborne.getVisual_thema());
+           System.out.println(soulsborne.isOwn_parry_mechanic());
+           System.out.println();
+
+       }
+
 
 
 
