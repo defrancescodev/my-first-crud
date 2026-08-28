39887210a47359765280f57264b8c21bdcca0a6a
Samuel De Francesco <defrancescodev@gmail.com>
2026-08-28 15:09:17 -0300

    feat(ResidentEvilDAO): 'getResidentEvilS()' method added to 'ResidentEvilDAO'

--- src/br/com/myfirstcrud/dao/ResidentEvilDAO.java
+++ src/br/com/myfirstcrud/dao/ResidentEvilDAO.java
@@ -5,6 +5,8 @@ import br.com.myfirstcrud.model.ResidentEvil;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ResidentEvilDAO {
@@ -48,6 +50,48 @@ public class ResidentEvilDAO {
     }
 
     public List<ResidentEvil> getResidentEvilS() {
-        String sql = "SELECT * FROM ";
+        String sql = "SELECT * FROM resident_evil ";
+        List<ResidentEvil> residentEvilList = new ArrayList<ResidentEvil>();
+        Connection con = null;
+        PreparedStatement pstm = null;
+        ResultSet rset = null;
+
+        try {
+
+            con = ConnectionFactory.createConnection();
+            pstm = (PreparedStatement) con.prepareStatement(sql);
+            rset = pstm.executeQuery();
+
+            while (rset.next()) {
+                ResidentEvil residentEvil = new ResidentEvil();
+                residentEvil.setTitle(rset.getString("title"));
+                residentEvil.setPrice(rset.getDouble("price"));
+                residentEvil.setDeveloper(rset.getString("developer"));
+                residentEvil.setRelease_year(rset.getInt("release_year"));
+                residentEvil.setMain_characte(rset.getString("main_character"));
+                residentEvilList.add(residentEvil);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (con != null) {
+                    con.close();
+                }
+
+                if (pstm != null) {
+                    pstm.close();
+                }
+
+                if (rset != null) {
+                    rset.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        return residentEvilList;
     }
 }
