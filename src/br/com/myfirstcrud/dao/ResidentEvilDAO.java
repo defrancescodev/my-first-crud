ad91b052510c41bf7c7c2f732b38760d9b0050c7
Samuel De Francesco <defrancescodev@gmail.com>
2026-08-28 14:23:24 -0300

    feat(ResidentEvil): added setters and getters

---
+++ src/br/com/myfirstcrud/dao/ResidentEvilDAO.java
@@ -0,0 +1,4 @@
+package br.com.myfirstcrud.dao;
+
+public class ResidentEvilDAO {
+}
--- src/br/com/myfirstcrud/model/ResidentEvil.java
+++ src/br/com/myfirstcrud/model/ResidentEvil.java
@@ -1,4 +1,58 @@
 package br.com.myfirstcrud.model;
 
 public class ResidentEvil {
+    private int id;
+    private String title;
+    private double price;
+    private String developer;
+    private int release_year;
+    private String protagonist;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDeveloper() {
+        return developer;
+    }
+
+    public void setDeveloper(String developer) {
+        this.developer = developer;
+    }
+
+    public int getRelease_year() {
+        return release_year;
+    }
+
+    public void setRelease_year(int release_year) {
+        this.release_year = release_year;
+    }
+
+    public String getProtagonist() {
+        return protagonist;
+    }
+
+    public void setProtagonist(String protagonist) {
+        this.protagonist = protagonist;
+    }
 }
